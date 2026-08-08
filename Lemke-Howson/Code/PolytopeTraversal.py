63c31bdb48d581d2352758080ecb9ce2e376d8c1
kostis1101 <csd5441@csd.uoc.gr>
2026-08-08 17:02:13 +0300

    polytope traversal changes

--- Lemke-Howson/Code/PolytopeTraversal.py
+++ Lemke-Howson/Code/PolytopeTraversal.py
@@ -1,12 +1,78 @@
+
 from manim import*
 from FixedText import*
 from manim.utils.rate_functions import ease_in_out_sine
 from manim.utils.rate_functions import ease_out_cubic
+import math
+import numpy as np
+from manim.typing import Point3DLike
 
+
+class CustomLine3D(Line3D):
+    def __init__(
+        self,
+        start: Point3DLike = LEFT,
+        end: Point3DLike = RIGHT,
+        thickness: float = 0.02,
+        color: ParsableManimColor | None = None,
+        resolution: int | tuple[int, int] = 24,
+        **kwargs: Any,
+    ):
+        super().__init__(start=start, end=end, thickness=thickness, color=color, resolution=resolution, **kwargs)
+        self.cl_settings = kwargs
+        self.cl_color = color
+
+    def change_end_points(
+        self, start: Point3DLike, end: Point3DLike
+    ) -> None:
+        self.set_start_and_end_attrs(start, end, **self.cl_settings)
+        self.set_color(self.cl_color)
+
+
+class Test(ThreeDScene):
+
+    def construct(self):
+        line = CustomLine3D((0, 0, 0), (1, 0, 0), color=YELLOW, thickness=0.05)
+        self.add(line)
+        self.play(line.animate.change_end_points((0, 1, 0), (2, 0, 0)))
+        self.wait()
 
 class PolytopeTraversal(ThreeDScene):
+
+    def camera_pos(self):
+        c = self.camera._frame_center.get_center()
+        d = self.camera.get_focal_distance()
+        phi = self.camera.get_phi()
+        theta = self.camera.get_theta()
+
+        return d * np.array([math.sin(phi) * math.cos(theta), math.sin(phi) * math.sin(theta), math.cos(phi)]) + c
+
+    def project_point(self, point, distance=5):
+        p = self.camera_pos()
+        R = self.camera.generate_rotation_matrix()
+
+        print(R)
+
+        point = np.linalg.inv(R) @ (point - p)
+        point = point / point[2] * distance
+
+        return p + R @ point
+
+    def project_mobject(self, mobject, distance=5):
+        return mobject.animate.move_to(self.project_point(mobject.get_center(), distance))
+
+    def project_line3d(self, line, distance=5):
+        s = self.project_point(line.get_start(), distance)
+        e = self.project_point(line.get_end(), distance)
+
+        return line.animate.change_end_points(s, e)
+        # return line.animate.set_points_by_ends(s, e)
+
+
     def construct(self):
- 
+
+        self.camera.should_apply_shading = False
+
         self.set_camera_orientation(
             phi=0 * DEGREES,
             theta=-90 * DEGREES,
@@ -140,19 +206,19 @@ class PolytopeTraversal(ThreeDScene):
         intersection_12 = Dot3D(
             point=axes.c2p(1 / 3, 11 / 3) + OUT * 0.07,
             radius=0.065,
-            color=GREEN,
+            color=GREEN
         )
  
         intersection_13 = Dot3D(
             point=axes.c2p(5 / 8, 5 / 2) + OUT * 0.07,
             radius=0.06,
-            color=GREEN,
+            color=GREEN
         )
  
         intersection_23 = Dot3D(
             point=axes.c2p(4 / 5, 16 / 5) + OUT * 0.07,
             radius=0.065,
-            color=GREEN,
+            color=GREEN
         )
  
         intersections = VGroup(
@@ -187,7 +253,7 @@ class PolytopeTraversal(ThreeDScene):
             ),
             run_time=0.65,
         )
- 
+
         self.wait(1)
  
         self.play(
@@ -199,6 +265,7 @@ class PolytopeTraversal(ThreeDScene):
  
         edge_color = YELLOW
         edge_width = 8
+        poly_line_width = 0.05
         depth = OUT * 0.12
  
         left_point = axes.c2p(0, 5) + depth
@@ -206,25 +273,25 @@ class PolytopeTraversal(ThreeDScene):
         middle_right_point = axes.c2p(4 / 5, 16 / 5) + depth
         right_point = axes.c2p(1, 4) + depth
  
-        finite_edge_1 = Line(
+        finite_edge_1 = CustomLine3D(
             left_point,
             middle_left_point,
             color=edge_color,
-            stroke_width=edge_width,
+            thickness=poly_line_width
         )
  
-        finite_edge_2 = Line(
+        finite_edge_2 = CustomLine3D(
             middle_left_point,
             middle_right_point,
             color=edge_color,
-            stroke_width=edge_width,
+            thickness=poly_line_width
         )
  
-        finite_edge_3 = Line(
+        finite_edge_3 = CustomLine3D(
             middle_right_point,
             right_point,
             color=edge_color,
-            stroke_width=edge_width,
+            thickness=poly_line_width
         )
  
         finite_chain = VGroup(
@@ -232,6 +299,7 @@ class PolytopeTraversal(ThreeDScene):
             finite_edge_2,
             finite_edge_3,
         )
+        finite_chain.set_z_index(2)
  
         vertex_radius = 0.13
  
@@ -239,24 +307,28 @@ class PolytopeTraversal(ThreeDScene):
             point=left_point,
             radius=vertex_radius,
             color=WHITE,
+            shade_in_3d=False
         )
  
         middle_left_vertex = Dot3D(
             point=middle_left_point,
             radius=vertex_radius,
             color=WHITE,
+            shade_in_3d=False
         )
  
         middle_right_vertex = Dot3D(
             point=middle_right_point,
             radius=vertex_radius,
             color=WHITE,
+            shade_in_3d=False
         )
  
         right_vertex = Dot3D(
             point=right_point,
             radius=vertex_radius,
             color=WHITE,
+            shade_in_3d=False
         )
  
         finite_vertices = VGroup(
@@ -266,6 +338,8 @@ class PolytopeTraversal(ThreeDScene):
             right_vertex,
         )
  
+        finite_vertices.set_z_index(3)
+
         self.play(
             LaggedStart(
                 Create(finite_edge_1),
@@ -291,20 +365,20 @@ class PolytopeTraversal(ThreeDScene):
         self.wait(0.6)
  
  
-        visible_ray_length = 9
+        visible_ray_length = 1000
  
-        left_visible_ray = Line(
+        left_visible_ray = CustomLine3D(
             left_point,
             left_point + UP * visible_ray_length,
             color=edge_color,
-            stroke_width=edge_width,
+            thickness=poly_line_width,
         )
  
-        right_visible_ray = Line(
+        right_visible_ray = CustomLine3D(
             right_point,
             right_point + UP * visible_ray_length,
             color=edge_color,
-            stroke_width=edge_width,
+            thickness=poly_line_width,
         )
  
         self.play(
@@ -393,28 +467,10 @@ class PolytopeTraversal(ThreeDScene):
             )
             floor_grid.add(line)
             cursor += raw * rung_scale
- 
+
+        floor_grid.set_z_index(-1)
         self.add(floor_grid)
  
-        #infinite rays
-
-        rail_total_length = 10000
-        rail_segments = 40
-        rail_growth = 1.35
-        rail_color_bias = 3  
-        edge_color_dim = interpolate_color(edge_color, BLACK, 0.45)
- 
-        left_rail = infinite_rail(
-            left_point, UP, rail_total_length, rail_segments,
-            rail_growth, edge_color, edge_color_dim, edge_width, rail_color_bias,
-        )
- 
-        right_rail = infinite_rail(
-            right_point, UP, rail_total_length, rail_segments,
-            rail_growth, edge_color, edge_color_dim, edge_width, rail_color_bias,
-        )
- 
-       
         lane_offset = 1.1
         lane_length = 300
         left_lane_dir = normalize(left_point - right_point)
@@ -436,43 +492,49 @@ class PolytopeTraversal(ThreeDScene):
             dash_length=0.35,
             dashed_ratio=0.5,
         )
- 
-        self.remove(
-            left_visible_ray,
-            right_visible_ray,
-        )
- 
+
         self.add(
-            left_rail,
-            right_rail,
             left_lane,
             right_lane,
         )
  
+        print(axes.c2p(1, 0) - axes.c2p(0, 0))
+
+        new_camera_origin = np.array([0, 0, 1]) * (axes.c2p(1, 0) - axes.c2p(0, 0))[0] / 2 + DOWN * 1.5
+        print(new_camera_origin)
        
         # camera movement
         self.move_camera(
-            phi=60 * DEGREES,
+            phi=90 * DEGREES,
             theta=-90 * DEGREES,
-            zoom=0.8,
-            frame_center=UP * 0.4,
-            focal_distance=2.2,
+            frame_center= new_camera_origin,
+            focal_distance=3,
             run_time=2.4,
+            zoom=1.3,
             rate_func=ease_in_out_sine,
         )
- 
         self.wait(0.45)
- 
-       #forward glide
-        self.move_camera(
-            phi=83 * DEGREES,
-            theta=-90 * DEGREES,
-            zoom=1.2,
-            frame_center=UP * 10.9,
-            focal_distance=4.2,
-            run_time=3,
-            rate_func=ease_in_out_sine,
+
+        infinite_vertex = Dot3D(point=new_camera_origin + UP * 3, radius=vertex_radius, color=WHITE)
+        infinite_vertex.set_z_index(3)
+
+        self.play(FadeIn(infinite_vertex))
+
+        self.wait()
+
+        print(self.camera_pos())
+        print(self.camera.get_rotation_matrix())
+        self.play(FadeOut(floor_grid, left_lane, right_lane))
+
+        self.play(
+            *[self.project_mobject(m) for m in finite_vertices],
+            self.project_mobject(infinite_vertex),
+            *[self.project_line3d(line) for line in finite_chain],
+            *[self.project_line3d(line) for line in [left_visible_ray, right_visible_ray]]
         )
- 
-        self.wait(1.2)
+        self.wait()
+
+        polygon = VGroup(*finite_vertices, infinite_vertex, *finite_chain, left_visible_ray, right_visible_ray)
+        self.play(Rotate(polygon, angle=-135 * DEGREES, axis=UP))
+        self.wait()
 
